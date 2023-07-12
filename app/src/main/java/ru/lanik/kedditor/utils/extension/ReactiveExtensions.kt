b9364e4a56e78aeeca2c31f2b4d099d979c5ffad
Lanik <daniilt971@gmail.com>
2023-07-13 02:06:16 +0300

    app: src: ext: Add suspend await extension for single rx

--- app/src/main/java/ru/lanik/kedditor/utils/extension/ReactiveExtensions.kt
+++ app/src/main/java/ru/lanik/kedditor/utils/extension/ReactiveExtensions.kt
@@ -2,7 +2,10 @@ package ru.lanik.kedditor.utils.extension
 
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
 import ru.lanik.kedditor.utils.SchedulerPolicy
+import kotlin.coroutines.resumeWithException
 
 fun <T : Any> Single<T>.applySchedulerPolicy(scheduler: SchedulerPolicy): Single<T> {
     return this.subscribeOn(scheduler.backThread())
@@ -16,4 +19,12 @@ fun <T : Any> Observable<T>.applySchedulerPolicy(scheduler: SchedulerPolicy): Ob
 
 fun <T : Any> Observable<T>.performOnBack(scheduler: SchedulerPolicy): Observable<T> {
     return this.subscribeOn(scheduler.backThread())
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun <T : Any> Single<T>.await(): T {
+    return suspendCancellableCoroutine { cont ->
+        val disposable = subscribe({ cont.resume(it) {} }, { cont.resumeWithException(it) })
+        cont.invokeOnCancellation { disposable.dispose() }
+    }
 }
